ff6c6bb4cda0b2f7aca340bf8b5e978b74fed38b
juamornav3 <92048474+juamornav3@users.noreply.github.com>
2024-03-25 21:15:08 +0100

    Proyecto base conectado a MongoDB

---
+++ pokedex/pokecursor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PokecursorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pokecursor'
---
+++ pokedex/pokecursor/models.py
@@ -0,0 +1,16 @@
+
+from django.db import models
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=255)
+    type_1 = models.CharField(max_length=255)
+    type_2 = models.CharField(max_length=255, blank=True, null=True)
+    total = models.IntegerField()
+    hp = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    sp_atk = models.IntegerField()
+    sp_def = models.IntegerField()
+    speed = models.IntegerField()
+    generation = models.IntegerField()
+    legendary = models.BooleanField()
