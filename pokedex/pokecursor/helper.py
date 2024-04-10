f5bff824a38074ee04c7dd151f72498c546f94f2
juamornav3 <juamornav3@alum.us.es>
2024-04-10 10:21:59 +0200

    fix:Filtro booleano arreglado
    Co-authored-by: Olegario Morato Navarro <olemornav@users.noreply.github.com>

--- pokedex/pokecursor/helper.py
+++ pokedex/pokecursor/helper.py
@@ -14,18 +14,25 @@ def list_all_pokemon_without_cursor():
     time_query = time.time() - start_time
     return results, round(time_query, 4)
 
+from django.db.models import Q
+
 def filter_pokemon_without_cursor(pokemon_name, type1, type2, generation, legendary):
     start_time = time.time()
-    results = Pokemon.objects.all().values()
+    results = Pokemon.objects.all()
     if pokemon_name:
-        results = results.filter(name__icontains=pokemon_name).values()
+        results = results.filter(name__icontains=pokemon_name)
     if type1:
-        results = results.filter(type_1=type1).values()
+        results = results.filter(type_1=type1)
     if type2:
-        results = results.filter(type_2=type2).values()
+        results = results.filter(type_2=type2)
     if generation:
-        results = results.filter(generation=generation).values()
+        results = results.filter(generation=generation)
+    list_results = list(results.values())
     if legendary:
-        results = results.filter(legendary=legendary).values()
+            print("Entro")
+            list_results = [res for res in results if res.legendary is True]
+    else:
+            list_results = [res for res in results if res.legendary is False]
+
     time_query = time.time() - start_time
-    return list(results), round(time_query, 4)
+    return list_results, round(time_query, 4)
